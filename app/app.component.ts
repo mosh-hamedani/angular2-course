3fdb69bf35a6a7deec955d84a8b66f541a0c6fa8
Mosh Hamedani <moshfegh@live.com.au>
2016-02-16 11:50:21 +1100

    Add a Bootstrap navbar.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,10 @@
 import {Component} from 'angular2/core';
 
+import {NavBarComponent} from './navbar.component';
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<navbar></navbar>',
+    directives: [NavBarComponent]
 })
 export class AppComponent { }
