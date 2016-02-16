7020f3fcf994240cadb2a32985d03bfe2e0fe548
Mosh Hamedani <moshfegh@live.com.au>
2016-02-16 15:05:33 +1100

    Highlight the current menu in navbar.

--- app/navbar.component.ts
+++ app/navbar.component.ts
@@ -1,9 +1,17 @@
 import {Component} from 'angular2/core';
-import {ROUTER_DIRECTIVES} from 'angular2/router';
+import {ROUTER_DIRECTIVES, Router} from 'angular2/router';
 
 @Component({
     selector: 'navbar',
     templateUrl: 'app/navbar.component.html',
-    directives: [ROUTER_DIRECTIVES] 
+    directives: [ROUTER_DIRECTIVES]
 })
-export class NavBarComponent { }
+export class NavBarComponent {
+    constructor(private _router: Router){
+    }
+    
+    isCurrentRoute(route){
+        var instruction = this._router.generate(route);
+        return this._router.isRouteActive(instruction);
+    }
+ }
