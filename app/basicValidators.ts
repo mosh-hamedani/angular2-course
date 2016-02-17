45dd8dba0c6028685415e51285c7339e8fca5138
Mosh Hamedani <moshfegh@live.com.au>
2016-02-17 11:38:30 +1100

    Implement validation.

---
+++ app/basicValidators.ts
@@ -0,0 +1,9 @@
+import {Control} from 'angular2/common';
+
+export class BasicValidators{
+    static email(control: Control){
+        var regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        var valid = regEx.test(control.value);
+        return valid ? null : { email: true };
+    }
+}
--- app/user-form.component.ts
+++ app/user-form.component.ts
@@ -1,5 +1,7 @@
 import {Component} from 'angular2/core';
-import {FormBuilder, ControlGroup} from 'angular2/common';
+import {FormBuilder, ControlGroup, Validators} from 'angular2/common';
+
+import {BasicValidators} from './basicValidators';
 
 @Component({
     templateUrl: 'app/user-form.component.html'
@@ -9,8 +11,8 @@ export class UserFormComponent {
 
 	constructor(fb: FormBuilder) {
 		this.form = fb.group({
-			name: [],
-			email: [],
+			name: ['', Validators.required],
+			email: ['', BasicValidators.email],
 			phone: [],
 			address: fb.group({
 				street: [],
