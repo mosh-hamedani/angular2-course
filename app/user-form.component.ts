29bbb8b2d8961321557f487c1b6203d1ce500280
Mosh Hamedani <moshfegh@live.com.au>
2016-02-17 13:08:46 +1100

    Create a user.

--- app/user-form.component.ts
+++ app/user-form.component.ts
@@ -1,16 +1,22 @@
 import {Component} from 'angular2/core';
 import {FormBuilder, ControlGroup, Validators} from 'angular2/common';
-import {CanDeactivate} from 'angular2/router';
+import {CanDeactivate, Router} from 'angular2/router';
 
 import {BasicValidators} from './basicValidators';
+import {UserService} from './user.service';
 
 @Component({
-    templateUrl: 'app/user-form.component.html'
+    templateUrl: 'app/user-form.component.html',
+    providers: [UserService]
 })
 export class UserFormComponent implements CanDeactivate {
 	form: ControlGroup;
 
-	constructor(fb: FormBuilder) {
+	constructor(
+        fb: FormBuilder,
+        private _router: Router,
+        private _userService: UserService
+    ) {
 		this.form = fb.group({
 			name: ['', Validators.required],
 			email: ['', BasicValidators.email],
@@ -29,5 +35,14 @@ export class UserFormComponent implements CanDeactivate {
 			return confirm('You have unsaved changes. Are you sure you want to navigate away?');
 
 		return true; 
+	}
+    
+    save(){
+		this._userService.addUser(this.form.value)
+            .subscribe(x => {
+                // Ideally, here we'd want:
+                // this.form.markAsPristine();
+                    this._router.navigate(['Users']);
+            });
 	}
 }
--- app/user.service.ts
+++ app/user.service.ts
@@ -13,4 +13,9 @@ export class UserService {
 		return this._http.get(this._url)
 			.map(res => res.json());
 	}
+    
+    addUser(user){
+		return this._http.post(this._url, JSON.stringify(user))
+			.map(res => res.json());
+	}
 }
